15425dd82833e45795483bb575ea91bfa9c4ebe3
unknown <polianaperez31@gmail.com>
2026-03-10 09:07:41 -0300

    exercicio

---
+++ aula3.py
@@ -0,0 +1,15 @@
+#alunos= [" "] * 5
+#for cont in range(0, len(alunos)):
+    #alunos[cont]= int (input ("informe um valor: "))
+
+#while(alunos != 0):
+ #print(alunos[cont])
+    #break;
+
+vetor= [0] * 5
+
+for cont in range(0, len(vetor)):
+    vetor[cont] = int(input("Informe um valor: "))
+
+for cont in range(0, len(vetor)):
+    print("Valor: ", vetor[cont])
---
+++ github.py
@@ -0,0 +1,22 @@
+import os
+print("Configurando email")
+comando_email= "git config user.email \"polianaperez31@gmail.com\" "
+os.system(comando_email)
+
+print("Adicionando modificações")
+comando1= "git add *"
+os.system(comando1)
+
+mensagem= input("Mensagem do commit: ")
+
+while(len(mensagem) < 5):
+    print("Mensagem muito pequena, detalhe mais")
+    mensagem = input("Mensagem do commit: ")
+
+print("Registrando alterações")
+comando2= "git commit -m " + mensagem 
+os.system(comando2)
+
+print("Enviando projeoto ao github")
+comando3= "git push"
+os.system(comando3)
